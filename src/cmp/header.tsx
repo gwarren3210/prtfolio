7eb01ce7ae9e9c7255e56277da263e620ce25ff7
gwarren3210 <gavrielewarren@gmail.com>
2023-07-05 20:19:00 -0400

    header

---
+++ src/cmp/header.tsx
@@ -0,0 +1,38 @@
+import styles from '@/app/page.module.css'
+import React from 'react';
+import { IconType } from 'react-icons';
+import { FaLinkedin, FaGithub, FaTwitter, FaInstagram, FaEnvelope, FaDiscord, } from 'react-icons/fa';
+import socialMedia from '@/data/socialMedia.json';
+import { SocialMediaLink } from '@/data/types';
+
+const iconComponents: { [key: string]: IconType } = {
+    FaLinkedin: FaLinkedin,
+    FaGithub: FaGithub,
+    FaTwitter: FaTwitter,
+    FaInstagram: FaInstagram,
+    FaEnvelope: FaEnvelope,
+    FaDiscord: FaDiscord,
+};
+
+export default function Header() {
+    return (
+        <header>
+            <nav>
+                <ul className={styles.header}>
+                    {socialMedia.map((link: SocialMediaLink, index: number) => {
+                        const IconComponent = iconComponents[link.icon];
+                        if (!IconComponent) return null;
+                        return (
+                            <li key={index}>
+                                <a href={link.url} target="_blank" rel="noreferrer">
+                                    <IconComponent size={30}/>
+                                </a>
+                            </li>
+                        );
+                    })}
+                    <li>check my resume for my number ; &#x29;, I reply faster to texts</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
